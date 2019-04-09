dc97f48a2b2c4dfad66c34db157cfebf0f8ff8c7
sandeepsajan0 <sandeepsajan0@gmail.com>
2019-04-10 03:35:27 +0530

    update url

--- inventoryApp/views.py
+++ inventoryApp/views.py
@@ -40,7 +40,7 @@ def bookslist(request):
         """
         q=Books(title=request.GET.get('booktitle'),readlink=request.GET.get('booklink'),copies=1,bookid=request.GET.get('bookid'))
         q.save()
-        return redirect('')
+        return redirect('/')
 
     if request.GET.get('AddCopy'):
         """
@@ -51,7 +51,7 @@ def bookslist(request):
         copies+=1
         book.copies = copies
         book.save()
-        return redirect('')
+        return redirect('/')
 
     if request.GET.get('Delete'):
         """
