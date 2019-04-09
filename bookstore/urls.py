115b0bbfcd7facbb706053aad5b4a0b55c1b25a3
sandeepsajan0 <sandeepsajan0@gmail.com>
2019-04-10 03:31:45 +0530

    update host

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-    path('/', include('inventoryApp.urls')),
+    path('', include('inventoryApp.urls')),
 ]
